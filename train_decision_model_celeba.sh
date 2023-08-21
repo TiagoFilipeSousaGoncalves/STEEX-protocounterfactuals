a10054f2dd8ef8f3c52481fa6c4ad0eacc494fcc
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2023-08-21 17:26:40 +0100

    Updated: train_decision_model_celeba.sh, train_decision_model_celebamaskhq.sh

--- train_decision_model_celeba.sh
+++ train_decision_model_celeba.sh
@@ -10,6 +10,6 @@
 
 echo "STEEX-Protocounterfactuals | Train Decision Model CelebA | Started"
 
-python train_decision_model_celeba.py --results_dir '/nas-ctm01/homes/tgoncalv/STEEX-protocounterfactuals/results' --images_dir '/nas-ctm01/datasets/public/BIOMETRICS/celeba-db/Img' --images_subdir 'img_align_squared128_celeba' --eval_dir '/nas-ctm01/datasets/public/BIOMETRICS/celeba-db/Eval' --anno_dir '/nas-ctm01/datasets/public/BIOMETRICS/celeba-db/Anno' --decision_model_name 'decision_model_celeba' --load_size 128 128 --train_attributes_idx 20 31 39 --batch_size 32 --optimizer 'Adam' --lr 0.0001 --step_size 10 --gamma_scheduler 0.5 --num_epochs 5
+python train_decision_model.py --dataset_name 'CelebaDB' --results_dir '/nas-ctm01/homes/tgoncalv/STEEX-protocounterfactuals/results' --images_dir '/nas-ctm01/datasets/public/BIOMETRICS/celeba-db/Img' --images_subdir 'img_align_squared128_celeba' --eval_dir '/nas-ctm01/datasets/public/BIOMETRICS/celeba-db/Eval' --anno_dir '/nas-ctm01/datasets/public/BIOMETRICS/celeba-db/Anno' --decision_model_name 'decision_model_celeba' --load_size 128 128 --train_attributes_idx 20 31 39 --batch_size 32 --optimizer 'Adam' --lr 0.0001 --step_size 10 --gamma_scheduler 0.5 --num_epochs 5
 
 echo "STEEX-Protocounterfactuals | Train Decision Model CelebA | Finished"
