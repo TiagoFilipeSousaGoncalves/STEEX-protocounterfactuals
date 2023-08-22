a09424678b339838a333e0d5f4b13e8c417c24a7
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2023-08-22 17:25:40 +0100

    Updated: code/data_seg_utilities.py, code/train_segmentation_model.py, train_segmentation_model_bdd100k.sh

---
+++ train_segmentation_model_bdd10k.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#SBATCH -p a100_80GB              # Partition
+#SBATCH --qos=a100                # QOS
+#SBATCH --job-name=steex_train_segmentation_model_bdd10k         # Job name
+#SBATCH -o slurm.%N.%j.out             # STDOUT
+#SBATCH -e slurm.%N.%j.err             # STDERR
+
+
+
+echo "STEEX-Protocounterfactuals | Train Segmentation Model BDD10kDB | Started"
+
+python code/train_segmentation_model.py --dataset_name 'BDD10kDB' --results_dir '/nas-ctm01/homes/tgoncalv/STEEX-protocounterfactuals/results' --images_dir '/nas-ctm01/datasets/public/bdd100k/images' --labels_dir '/nas-ctm01/datasets/public/bdd100k/labels' --n_classes 20 --segmentation_network_name 'deeplabv3_bdd10k' --seed 42 --batch_size 8 --num_epochs 50
+
+echo "STEEX-Protocounterfactuals | Train Segmentation Model BDD10kDB | Finished"
