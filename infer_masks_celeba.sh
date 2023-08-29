38e18f8fd9f80908483ecd40151ab61f22665fdb
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2023-08-29 12:29:58 +0100

    Updated: infer_masks_celeba.sh

--- infer_masks_celeba.sh
+++ infer_masks_celeba.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-#SBATCH -p v100_32GB              # Partition
-#SBATCH --qos=v100                # QOS
+#SBATCH -p a100_80GB              # Partition
+#SBATCH --qos=a100                # QOS
 #SBATCH --job-name=steex_infer_masks_celeba         # Job name
 #SBATCH -o slurm.%N.%j.out             # STDOUT
 #SBATCH -e slurm.%N.%j.err             # STDERR
@@ -10,6 +10,6 @@
 
 echo "STEEX-Protocounterfactuals | Infer Segmentation Masks CelebA | Started"
 
-python code/infer_masks.py --dataset_name 'CelebaDB' --results_dir '/nas-ctm01/homes/tgoncalv/STEEX-protocounterfactuals/results' --images_dir '/nas-ctm01/datasets/public/BIOMETRICS/celeba-db/Img' --images_subdir 'img_align_squared128_celeba' --eval_dir '/nas-ctm01/datasets/public/BIOMETRICS/celeba-db/Eval' --anno_dir '/nas-ctm01/datasets/public/BIOMETRICS/celeba-db/Anno' --save_dir_masks '/nas-ctm01/datasets/public/BIOMETRICS/celeba-db/Masks-DeepLabV3' --n_classes 19 --segmentation_network_name 'deeplabv3_celebamaskhq' --seed 42 --batch_size 16
+python code/infer_masks.py --dataset_name 'CelebaDB' --results_dir '/nas-ctm01/homes/tgoncalv/STEEX-protocounterfactuals/results' --images_dir '/nas-ctm01/datasets/public/BIOMETRICS/celeba-db/Img' --images_subdir 'img_align_squared128_celeba' --eval_dir '/nas-ctm01/datasets/public/BIOMETRICS/celeba-db/Eval' --anno_dir '/nas-ctm01/datasets/public/BIOMETRICS/celeba-db/Anno' --save_dir_masks '/nas-ctm01/datasets/public/BIOMETRICS/celeba-db/Masks-DeepLabV3' --n_classes 19 --segmentation_network_name 'deeplabv3_celebamaskhq' --seed 42 --batch_size 32
 
 echo "STEEX-Protocounterfactuals | Infer Segmentation Masks CelebA | Finished"
