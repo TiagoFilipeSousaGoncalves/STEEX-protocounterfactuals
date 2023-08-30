115111ead4adb5672ffe106e097914eefb82ce62
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2023-08-30 11:23:55 +0100

    Updated: train_sean_autoencoder_bddoia.sh

--- train_sean_autoencoder_bddoia.sh
+++ train_sean_autoencoder_bddoia.sh
@@ -10,6 +10,6 @@
 
 echo "STEEX-Protocounterfactuals | Train SEAN Autoencoder BDDOIADB | Started"
 
-python code/train_sean_autoencoder.py --name 'sean_autoencoder_bddoia' --gpu_ids '0' --checkpoints_dir '/nas-ctm01/homes/tgoncalv/STEEX-protocounterfactuals/results/checkpoints' --phase 'train' --batchSize 8 --load_size 512 --crop_size 512 --aspect_ratio 2.0 --label_nc 19 --semantic_nc 20 --contain_dontcare_label --dataset_mode 'BDDOIADB' --data_dir '/nas-ctm01/datasets/public/bdd-oia/data' --metadata_dir '/nas-ctm01/datasets/public/bdd-oia/metadata' --nThreads 4 --augment
+python code/train_sean_autoencoder.py --name 'sean_autoencoder_bddoia' --gpu_ids '0' --checkpoints_dir '/nas-ctm01/homes/tgoncalv/STEEX-protocounterfactuals/results/checkpoints' --phase 'train' --batchSize 8 --load_size 512 --crop_size 512 --aspect_ratio 2.0 --label_nc 19 --semantic_nc 20 --contain_dontcare_label --dataset_mode 'BDDOIADB' --data_dir '/nas-ctm01/datasets/public/bdd-oia/data' --metadata_dir '/nas-ctm01/datasets/public/bdd-oia/metadata' --nThreads 4 --augment --no_instance
 
 echo "STEEX-Protocounterfactuals | Train Decision Model BDDOIADB | Finished"
