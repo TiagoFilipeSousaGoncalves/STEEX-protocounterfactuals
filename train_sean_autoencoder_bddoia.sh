f53b5b6bfb3659bf7a19507b1b62e4f3077a521e
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2023-09-05 16:13:53 -0400

    Updated: train_sean_autoencoder_bddoia.sh

--- train_sean_autoencoder_bddoia.sh
+++ train_sean_autoencoder_bddoia.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-#SBATCH -p a100_80GB              # Partition
-#SBATCH --qos=a100                # QOS
+#SBATCH -p rtx6000_24GB              # Partition
+#SBATCH --qos=rtx6000                # QOS
 #SBATCH --job-name=steex_train_sean_autoencoder_bddoia         # Job name
 #SBATCH -o slurm.%N.%j.out             # STDOUT
 #SBATCH -e slurm.%N.%j.err             # STDERR
@@ -10,6 +10,6 @@
 
 echo "STEEX-Protocounterfactuals | Train SEAN Autoencoder BDDOIADB | Started"
 
-python code/train_sean_autoencoder.py --name 'sean_autoencoder_bddoia' --gpu_ids '0' --checkpoints_dir '/nas-ctm01/homes/tgoncalv/STEEX-protocounterfactuals/results/checkpoints' --phase 'train' --batchSize 8 --load_size 512 --crop_size 512 --aspect_ratio 2.0 --label_nc 19 --semantic_nc 20 --contain_dontcare_label --dataset_mode 'BDDOIADB' --data_dir '/nas-ctm01/datasets/public/bdd-oia/data' --metadata_dir '/nas-ctm01/datasets/public/bdd-oia/metadata' --nThreads 4 --augment --no_instance
+python code/train_sean_autoencoder.py --name 'sean_autoencoder_bddoia' --gpu_ids '0' --checkpoints_dir '/nas-ctm01/homes/tgoncalv/STEEX-protocounterfactuals/results/checkpoints' --phase 'train' --batchSize 1 --load_size 512 --crop_size 512 --aspect_ratio 2.0 --label_nc 19 --semantic_nc 20 --contain_dontcare_label --dataset_mode 'BDDOIADB' --data_dir '/nas-ctm01/datasets/public/bdd-oia/data' --metadata_dir '/nas-ctm01/datasets/public/bdd-oia/metadata' --masks_dir '/nas-ctm01/datasets/public/bdd-oia/deeplabv3_masks' --nThreads 4 --augment --no_instance
 
 echo "STEEX-Protocounterfactuals | Train Decision Model BDDOIADB | Finished"
