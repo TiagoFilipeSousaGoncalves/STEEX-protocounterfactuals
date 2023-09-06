be6d07badb2efa84abf3b4825b945863f0c5b2bd
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2023-09-06 14:24:36 -0400

    Updated: code/data_utilities_sean.py, train_sean_autoencoder_bddoia.sh

--- code/data_utilities_sean.py
+++ code/data_utilities_sean.py
@@ -403,13 +403,15 @@ class BDDOIADB(Pix2pixDataset):
     @staticmethod
     def modify_commandline_options(parser, is_train):
         parser = Pix2pixDataset.modify_commandline_options(parser, is_train)
-        parser.set_defaults(preprocess_mode='resize_and_crop')
-        load_size = 286 if is_train else 256
-        parser.set_defaults(load_size=load_size)
-        parser.set_defaults(crop_size=256)
-        parser.set_defaults(display_winsize=256)
-        parser.set_defaults(label_nc=13)
-        parser.set_defaults(contain_dontcare_label=False)
+        
+        # TODO: Remove uppon testing
+        # parser.set_defaults(preprocess_mode='resize_and_crop')
+        # load_size = 286 if is_train else 256
+        # parser.set_defaults(load_size=load_size)
+        # parser.set_defaults(crop_size=256)
+        # parser.set_defaults(display_winsize=256)
+        # parser.set_defaults(label_nc=13)
+        # parser.set_defaults(contain_dontcare_label=False)
 
         # TODO: Additional CLI option
         # parser.add_argument('--data_dir', type=str, required=True, help='Path to the directory that contains the images.')
