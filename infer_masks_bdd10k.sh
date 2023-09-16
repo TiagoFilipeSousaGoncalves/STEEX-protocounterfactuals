05b4e67fc983b006820f224a1292556034a027fb
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2023-09-16 16:57:13 -0400

    Updated: infer_masks_bdd10k.sh, infer_masks_bddoia.sh

--- infer_masks_bdd10k.sh
+++ infer_masks_bdd10k.sh
@@ -10,7 +10,7 @@
 
 echo "STEEX-Protocounterfactuals | Infer Segmentation Masks BDD10kDB | Started"
 
-python code/infer_masks.py --dataset_name 'BDD10kDB' --results_dir '/nas-ctm01/homes/tgoncalv/STEEX-protocounterfactuals/results' --images_dir '/nas-ctm01/datasets/public/bdd100k/images' --labels_dir '/nas-ctm01/datasets/public/bdd100k/labels' --save_dir_masks '/nas-ctm01/datasets/public/bdd100k/labels/sem_seg_deeplabv3/train/' --n_classes 20 --segmentation_network_name 'deeplabv3_bdd10k' --seed 42 --batch_size 1 --subset 'train'
-python code/infer_masks.py --dataset_name 'BDD10kDB' --results_dir '/nas-ctm01/homes/tgoncalv/STEEX-protocounterfactuals/results' --images_dir '/nas-ctm01/datasets/public/bdd100k/images' --labels_dir '/nas-ctm01/datasets/public/bdd100k/labels' --save_dir_masks '/nas-ctm01/datasets/public/bdd100k/labels/sem_seg_deeplabv3/val/' --n_classes 20 --segmentation_network_name 'deeplabv3_bdd10k' --seed 42 --batch_size 1 --subset 'val'
+python code/infer_masks.py --dataset_name 'BDD10kDB' --results_dir '/nas-ctm01/homes/tgoncalv/STEEX-protocounterfactuals/results' --images_dir '/nas-ctm01/datasets/public/bdd100k/images' --labels_dir '/nas-ctm01/datasets/public/bdd100k/labels' --save_dir_masks '/nas-ctm01/datasets/public/bdd100k/labels/sem_seg_deeplabv3/train/' --n_classes 20 --segmentation_network_name 'deeplabv3_bdd10k' --seed 42 --batch_size 8 --subset 'train'
+python code/infer_masks.py --dataset_name 'BDD10kDB' --results_dir '/nas-ctm01/homes/tgoncalv/STEEX-protocounterfactuals/results' --images_dir '/nas-ctm01/datasets/public/bdd100k/images' --labels_dir '/nas-ctm01/datasets/public/bdd100k/labels' --save_dir_masks '/nas-ctm01/datasets/public/bdd100k/labels/sem_seg_deeplabv3/val/' --n_classes 20 --segmentation_network_name 'deeplabv3_bdd10k' --seed 42 --batch_size 8 --subset 'val'
 
 echo "STEEX-Protocounterfactuals | Infer Segmentation Masks BDD10kDB | Finished"
